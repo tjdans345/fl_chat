72852619173ef0d4d49384d54dd35b74b07d46c4
meteor <tjdans34568@gmail.com>
2024-02-21 13:57:39 +0900

    Feat : 채팅방 이슈 해결 진행중

--- lib/exception/custom_exception.dart
+++ lib/exception/custom_exception.dart
@@ -1,8 +1,6 @@
 import 'package:fl_chat/config/generic_builder.dart';
 
-
-
-class CustomException implements Exception{
+class CustomException implements Exception {
   late String errorType;
   late String errorMessage;
 
@@ -14,9 +12,17 @@ class CustomException implements Exception{
       errorType: json['errorType'] as String,
       errorMessage: json['errorMessage'] as String,
     );
+
   }
 
-
+  // // 정적 메서드를 만드는 방법
+  // CustomException.test() {
+  //   print("asdsadasd");
+  // }
+  // // 아래와 동일
+  // static void test() {
+  //   print("asdsadasd");
+  // }
 }
 
 // 불변성을 유지해야지 값이 변화되었을 때 상태 감지가 가능하다
--- lib/module/chat/service/chat_service.dart
+++ lib/module/chat/service/chat_service.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fl_chat/exception/custom_exception.dart';
 import 'package:fl_chat/module/chat/model/message_model.dart';
 
 class ChatService {
